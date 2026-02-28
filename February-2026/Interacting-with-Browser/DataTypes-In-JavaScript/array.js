a456ddbbe3f9d6d24f0a57b71c6229d39b6b0c3f
GitPirates <starshivamverma2@gmail.com>
2026-02-28 22:50:26 +0530

    feat: enhance array.js with key points on array methods and memory usage

--- February-2026/Interacting-with-Browser/DataTypes-In-JavaScript/array.js
+++ February-2026/Interacting-with-Browser/DataTypes-In-JavaScript/array.js
@@ -7,5 +7,10 @@ console.log(Array.isArray(trainCode))
 //  If you want to create a new array with a segment removed without changing the original, you should use toSpliced() (modern) or slice()/filter() instead.
 
 
-
-
+// Key Points
+// 1. prefer [] not Array(n) because it can result in memory issues causing hard coded memory sloting: Example [] uses memory according to need i.e. demand and supply. However, in Array() it allocates full memory
+// 2. arrays are 0 based.
+// 3. Arrays mutable methods => push, pop, shift, unshift, splice
+// 3. Arrays immutable methods => concat, slice, flat, flatmap eg: [1,2,3,4,[5,6]]
+// 4. Searching: includes()
+// 5. Array.isArray()
