f716dc49024447b680f5c65e1d82d8150fc6bb74
GitPirates <starshivamverma2@gmail.com>
2026-03-04 21:53:17 +0530

    feat: add duplicate prepareOrderCB function for asynchronous order preparation

--- February-2026/Interacting-with-Browser/Asynchronous-JavaScript/promises.js
+++ February-2026/Interacting-with-Browser/Asynchronous-JavaScript/promises.js
@@ -1,3 +1,6 @@
 function prepareOrderCB(dish, cb) {
   setTimeout(() => cb(null, { dish, status: "prepared" }), 100);
 }
+function prepareOrderCB(dish, cb) {
+  setTimeout(() => cb(null, { dish, status: "prepared" }), 100);
+}
