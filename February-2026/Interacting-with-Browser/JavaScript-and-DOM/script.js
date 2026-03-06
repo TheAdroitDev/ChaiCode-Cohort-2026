ebaf69fad59c060d1845214ec934d103551aab70
GitPirates <starshivamverma2@gmail.com>
2026-03-07 02:01:19 +0530

    feat: add square function with performance timing examples

---
+++ February-2026/Interacting-with-Browser/JavaScript-and-DOM/script.js
@@ -0,0 +1,16 @@
+function square(n){
+    return n*n;
+}
+
+console.time("Start Code");
+square(4344343443344461)
+console.timeEnd("Start Code");
+console.time("Start Code");
+square(4344343443344461)
+console.timeEnd("Start Code");
+console.time("Start Code");
+square(4344343443344461)
+console.timeEnd("Start Code");
+console.time("Start Code");
+square(4344343443344461)
+console.timeEnd("Start Code");
