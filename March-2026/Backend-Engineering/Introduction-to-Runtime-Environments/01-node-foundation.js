52a3765e1bb031546dfd74e7aae93bdba6f8527d
GitPirates <starshivamverma2@gmail.com>
2026-03-07 23:31:34 +0530

    feat: add foundational Node.js setup with file system, path, and OS modules

---
+++ March-2026/Backend-Engineering/Introduction-to-Runtime-Environments/01-node-foundation.js
@@ -0,0 +1,12 @@
+const fs = require("fs")
+const path = require("path")
+const os = require("os")
+
+
+/* console.log("Check Version Node: ",process.versions.node);
+console.log("Check Version V8:",process.versions.v8);
+console.log("Check Version libuv:",process.versions.uv);
+console.log("=======================");
+console.log("CPU:",os.cpus()); */
+
+
