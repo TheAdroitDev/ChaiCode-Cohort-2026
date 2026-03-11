6be5e943d2532f0b4a3dea05f2728003dbf9e364
GitPirates <starshivamverma2@gmail.com>
2026-03-11 22:14:53 +0530

    feat: add myScript.js with basic arithmetic functions

---
+++ March-2026/TypeScript-Essentials/JavaScript-with-Types/myScript.js
@@ -0,0 +1,10 @@
+function add(a, b) {
+    return a + b;
+}
+var result = add(3, 54);
+console.log(result);
+function subtract(a, b) {
+    return a - b;
+}
+var anotherResult = subtract(434349465, 554700);
+console.log(anotherResult);
