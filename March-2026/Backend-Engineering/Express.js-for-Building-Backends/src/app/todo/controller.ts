7817bd4d4d5e1d684c4b470a9547ec56f88acbd8
GitPirates <starshivamverma2@gmail.com>
2026-03-14 13:03:49 +0530

    feat: add TodoController with methods to get and insert todos

---
+++ March-2026/Backend-Engineering/Express.js-for-Building-Backends/src/app/todo/controller.ts
@@ -0,0 +1,31 @@
+import type { Request, Response } from "express";
+import { todoValidationSchema, type Todo } from "../models/todo.schema.js";
+
+
+class TodoController {
+    private _db: Todo[]
+
+    constructor() {
+        this._db = []
+    }
+
+    public handleGetAllTodos(req: Request, res: Response) {
+        const todos = this._db
+        return res.json({ todos })
+    }
+
+    public async handleInsertTodo(req: Request, res: Response) {
+        try {
+            const unValidated = req.body;
+
+            const validatedResult = await todoValidationSchema.parseAsync(unValidated);
+            this._db.push(validatedResult);
+
+            return res.status(201).json({ todo: validatedResult })
+        } catch (error) {
+            return res.status(501).json({ error })
+        }
+    }
+}
+
+export default TodoController;
