65ccc2f52f1815cdecb73b34b9cc0640692ae018
GitPirates <starshivamverma2@gmail.com>
2026-03-31 23:37:54 +0530

    feat: create route for auth

---
+++ March-2026/Backend-Engineering/User-Authentication/JS-MongoDB/src/modules/auth/auth.routes.js
@@ -0,0 +1,10 @@
+import * as controller from "./auth.controller.js"
+import { Router } from "express"
+import validate from "../../common/middleware/validate.middleware.js"
+import registerDto from "./dto/register.dto.js"
+
+const router = Router();
+
+router.post("/register", validate(registerDto), controller.register);
+
+export default router
