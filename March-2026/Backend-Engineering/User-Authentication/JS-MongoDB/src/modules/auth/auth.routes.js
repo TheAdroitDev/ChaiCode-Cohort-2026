aee80b434f2604dcef5bcb8a00ffeef4d8ffd3c0
GitPirates <starshivamverma2@gmail.com>
2026-04-02 18:27:05 +0530

    feat: complete authentication routes setup

--- March-2026/Backend-Engineering/User-Authentication/JS-MongoDB/src/modules/auth/auth.routes.js
+++ March-2026/Backend-Engineering/User-Authentication/JS-MongoDB/src/modules/auth/auth.routes.js
@@ -2,9 +2,18 @@ import * as controller from "./auth.controller.js"
 import { Router } from "express"
 import validate from "../../common/middleware/validate.middleware.js"
 import registerDto from "./dto/register.dto.js"
+import LoginDto from "./dto/login.dto.js"
 
 const router = Router();
 
 router.post("/register", validate(registerDto), controller.register);
+
+router.post("/login", validate(LoginDto), controller.login);
+
+router.get("/logout", authenticate(), controller.logout)
+
+router.get("/me", authenticate(), controller.getMe);
+
+router.get("/verify-email/:token", controller.verifyEmail); // seperate controller
 
 export default router
