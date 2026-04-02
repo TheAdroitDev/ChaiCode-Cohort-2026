9d664a2424f2dd55004783d14a665a0bc24f6c19
GitPirates <starshivamverma2@gmail.com>
2026-04-02 18:26:54 +0530

    feat: complete authentication controller

--- March-2026/Backend-Engineering/User-Authentication/JS-MongoDB/src/modules/auth/auth.controller.js
+++ March-2026/Backend-Engineering/User-Authentication/JS-MongoDB/src/modules/auth/auth.controller.js
@@ -4,7 +4,31 @@ import ApiResponse from "../../common/utils/api-response.js"
 const register = async (res, req) => {
     const user = await authService.register(req.body)
 
-    return ApiResponse.created(res, "Registration Sucessfully", user)
+    return ApiResponse.created(res, "Registration sucessfully", user)
 }
 
-export {register}
+const login = async (req, res) => {
+    const { user, accessToken, refreshToken } = await authService.login(req.body)
+
+    // set cookies
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secured: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000  // 7 days
+    })
+
+    ApiResponse.ok(res, "Login successfull", { user, accessToken })
+}
+
+const logout = async (req, res) => {
+    await authService.logout(req.user.id)
+    res.clearCookie("refreshToken")
+
+    ApiResponse.ok(res, "Logout successfull")
+}
+
+const getMe = async (req, res) => {
+    const user = await authService.getMe(req.user.id)
+    ApiResponse.ok(res, "User profile", user)
+}
+export { register, login, logout, getMe}
