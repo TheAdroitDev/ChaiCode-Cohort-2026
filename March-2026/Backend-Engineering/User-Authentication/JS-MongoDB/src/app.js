d577bc00f2c021c490509a8e2ec4423767329b46
GitPirates <starshivamverma2@gmail.com>
2026-04-02 18:26:38 +0530

    feat: complete app use cookie parser and accept json

--- March-2026/Backend-Engineering/User-Authentication/JS-MongoDB/src/app.js
+++ March-2026/Backend-Engineering/User-Authentication/JS-MongoDB/src/app.js
@@ -1,5 +1,14 @@
 import express from "express"
+import cookieParser from "cookie-parser"
+import authRoute from "../src/modules/auth/auth.routes.js"
 
 const app = express()
+app.use(express.json()) // accept json
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cookieParser())
+
+app.use("/api/auth", authRoute);
 
 export default app;
