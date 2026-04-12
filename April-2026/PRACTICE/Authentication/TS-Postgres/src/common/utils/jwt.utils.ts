e358029e02e1d77926fd50e36f995521e88cc0be
GitPirates <starshivamverma2@gmail.com>
2026-04-12 12:53:38 +0530

    feat: Jwt utility

---
+++ April-2026/PRACTICE/Authentication/TS-Postgres/src/common/utils/jwt.utils.ts
@@ -0,0 +1,67 @@
+import crypto from "crypto";
+import jwt from "jsonwebtoken";
+import type { JwtPayload } from "jsonwebtoken"
+import { env } from "../../app/env.js";
+import type { SignOptions } from "jsonwebtoken";
+
+// Define payload type
+type TokenPayload = {
+  userId: string;
+};
+
+type ResetToken = {
+  rawToken: string;
+  hashedToken: string;
+};
+
+
+// for lower verbose code: define a small helper type
+type Expiry = Exclude<SignOptions["expiresIn"], undefined>;
+
+export const generateAccessToken = (payload: TokenPayload): string => {
+  return jwt.sign(payload, env.JWT_ACCESS_SECRET, {
+
+    // exclude undefined optional if exactOptionalPropertyTypes: true in tsconfig.json
+
+    expiresIn: env.JWT_ACCESS_EXPIRY as Expiry
+  });
+};
+
+export const verifyAccessToken = (
+  token: string
+): JwtPayload | TokenPayload | null => {
+  try {
+    return jwt.verify(token, env.JWT_ACCESS_SECRET) as JwtPayload | TokenPayload;
+  } catch {
+    return null;
+  }
+};
+
+
+export const generateRefreshToken = (payload: TokenPayload): string => {
+  return jwt.sign(payload, env.JWT_REFRESH_SECRET, {
+    expiresIn: env.JWT_ACCESS_EXPIRY as Expiry
+  });
+};
+
+export const verifyRefreshToken = (
+  token: string
+): JwtPayload | TokenPayload | null => {
+  try {
+    return jwt.verify(token, env.JWT_REFRESH_SECRET) as JwtPayload | TokenPayload;
+  } catch {
+    return null;
+  }
+};
+
+
+export const generateResetToken = (): ResetToken => {
+  const rawToken = crypto.randomBytes(32).toString("hex");
+
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(rawToken)
+    .digest("hex");
+
+  return { rawToken, hashedToken };
+};
