f7fc943e3e05e997578d683218c31c82416f357d
GitPirates <starshivamverma2@gmail.com>
2026-04-14 15:15:38 +0530

    feat: complete signin controller with token

--- April-2026/PRACTICE/Authentication/TS-Postgres/src/app/auth/auth.controller.ts
+++ April-2026/PRACTICE/Authentication/TS-Postgres/src/app/auth/auth.controller.ts
@@ -1,11 +1,12 @@
 import type { Request, Response } from "express";
-import { signUpModel } from "./auth.model.js";
+import { signInModel, signUpModel } from "./auth.model.js";
 import { db } from "../../db/index.js";
 import { userTable } from "../../db/schema.js";
 import { eq } from "drizzle-orm";
 import ApiError from "../../common/utils/api-error.js";
 import { createHmac, randomBytes } from "node:crypto";
 import ApiResponse from "../../common/utils/api-response.js";
+import { generateAccessToken } from "../../common/utils/jwt.utils.js";
 
 
 class AuthenticationController {
@@ -30,7 +31,7 @@ class AuthenticationController {
 
         // validation
         if (userEmailResult) {
-            ApiError.conflict("User already exists")
+            throw ApiError.conflict("User already exists")
         }
 
         // prepare salt
@@ -47,7 +48,44 @@ class AuthenticationController {
             salt
         }).returning({ id: userTable.id })
 
-        ApiResponse.created(res, "User created successfully", result)
+        return ApiResponse.created(res, "User created successfully", result)
+    }
+
+    public async handleSignIn(req: Request, res: Response) {
+        // handle the data (DTO)
+        const validationResult = await signInModel.safeDecodeAsync(req.body)
+
+        // validation
+        if (validationResult.error) {
+            return res.json(400).json({
+                message: "Body validation failed!",
+                error: validationResult.error.issues
+            })
+        }
+
+        // extract the data
+        const { email, password } = validationResult.data
+
+
+        // validate user's email
+        const [userSelect] = await db.select().from(userTable).where(eq(userTable.email, email))
+
+        // validation
+        if (!userSelect) {
+            throw ApiError.notFound(`User with this email ${email} does not exists`)
+        }
+
+        // validate user's given password
+        const salt = userSelect?.salt!
+        // hash the password by matching the user salt from db
+        const hash = createHmac('sha256', salt).update(password).digest('hex')
+
+        if (userSelect?.password !== hash) throw ApiError.badRequest("Email or password is incorrect")
+
+        // token
+        const token = generateAccessToken({ userId: userSelect.id })
+        
+        return ApiResponse.signedIn(res, "Sign-in success", { token})
     }
 }
 
