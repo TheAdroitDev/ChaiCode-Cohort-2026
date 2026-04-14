d5d76e523a5ff6b1e34a362fff99b6edaac44e0d
GitPirates <starshivamverma2@gmail.com>
2026-04-14 15:58:23 +0530

    feat: add getMe route and restricted middleware

--- April-2026/PRACTICE/Authentication/TS-Postgres/src/app/auth/auth.routes.ts
+++ April-2026/PRACTICE/Authentication/TS-Postgres/src/app/auth/auth.routes.ts
@@ -1,5 +1,6 @@
 import express, { Router } from "express";
 import AuthenticationController from "./auth.controller.js"
+import { restrictToAuthenticatedUser } from "./middleware/auth.middleware.js";
 
 const authenticationController = new AuthenticationController()
 
@@ -8,3 +9,5 @@ export const authRouter: Router = express.Router()
 authRouter.post("/sign-up", authenticationController.handleSignUp.bind(authenticationController)) 
 // signin route 
 authRouter.post("/sign-in", authenticationController.handleSignIn.bind(authenticationController)) 
+
+authRouter.get("/me", restrictToAuthenticatedUser(),authenticationController.handleMe.bind(authenticationController))
