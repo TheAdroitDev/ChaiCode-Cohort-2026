91c3f820cc124f06fd161e97e1c98fa3628f3205
GitPirates <starshivamverma2@gmail.com>
2026-04-14 15:57:49 +0530

    feat: complete index file working auth

--- April-2026/PRACTICE/Authentication/TS-Postgres/src/app/index.ts
+++ April-2026/PRACTICE/Authentication/TS-Postgres/src/app/index.ts
@@ -3,14 +3,18 @@ import type { Express } from "express";
 import express from "express"
 import ApiResponse from "../common/utils/api-response.js";
 import { authRouter } from "./auth/auth.routes.js";
+import { authenticationMiddleware, restrictToAuthenticatedUser } from "./auth/middleware/auth.middleware.js";
 
 export function createApplication(): Express {
     const app = express()
 
     // middlewares
     app.use(express.json())
+
     app.use(cookieParser())
 
+    app.use(authenticationMiddleware())
+    
     // routes
     app.get("/", (req, res) => {
         return ApiResponse.ok(res, "Welcome to the continental");
