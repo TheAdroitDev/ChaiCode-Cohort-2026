96757b94f888922619c7a29b60866661b00bf113
GitPirates <starshivamverma2@gmail.com>
2026-04-14 22:18:24 +0530

    feat: add sql lab task 1

---
+++ April-2026/Labs/SQL/Querying-Data/task-1.sql
@@ -0,0 +1,9 @@
+-- SELECT All Columns
+-- Learn to retrieve all data from a table using SELECT *. The users table contains 15 customers with personal information.
+
+-- SELECT * retrieves every column from a table. We have 15 users in our database.
+-- Task: Select all columns from the users table to see customer data including id, name, email, phone, age, country, and city.
+-- Write your SQL query here
+
+
+SELECT * FROM USERS
