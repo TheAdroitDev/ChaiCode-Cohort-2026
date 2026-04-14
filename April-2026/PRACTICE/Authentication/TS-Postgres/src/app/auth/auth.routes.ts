dcc8264141b52b19786dc8fc6bc4f5141536424c
GitPirates <starshivamverma2@gmail.com>
2026-04-14 15:16:05 +0530

    feat: add signin route

--- April-2026/PRACTICE/Authentication/TS-Postgres/src/app/auth/auth.routes.ts
+++ April-2026/PRACTICE/Authentication/TS-Postgres/src/app/auth/auth.routes.ts
@@ -6,3 +6,5 @@ const authenticationController = new AuthenticationController()
 export const authRouter: Router = express.Router()
 
 authRouter.post("/sign-up", authenticationController.handleSignUp.bind(authenticationController)) 
+// signin route 
+authRouter.post("/sign-in", authenticationController.handleSignIn.bind(authenticationController)) 
