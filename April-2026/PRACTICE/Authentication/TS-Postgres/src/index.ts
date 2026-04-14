5d513fe576fb3de72d4843537edb3830725bdec4
GitPirates <starshivamverma2@gmail.com>
2026-04-14 14:40:41 +0530

    feat: server file complete

--- April-2026/PRACTICE/Authentication/TS-Postgres/src/index.ts
+++ April-2026/PRACTICE/Authentication/TS-Postgres/src/index.ts
@@ -1,11 +1,17 @@
 import type { Express } from "express";
 import { createServer } from "node:http";
+import { env } from "./app/env.js";
+import { createApplication } from "./app/index.js";
 
 export function main() {
     try {
-        const server = createServer()
+        const server = createServer(createApplication())
+        const PORT: number = env.PORT || 3000;
 
-        const PORT = 3000
+        server.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        })
+
     } catch (error: any) {
         console.log("Error Starting http server", error);
         // terminate the server on error
