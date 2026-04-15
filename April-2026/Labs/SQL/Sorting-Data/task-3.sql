5af6aeb67dbf9663ee9a0e2923c8c17edc2d6c39
GitPirates <starshivamverma2@gmail.com>
2026-04-15 15:01:48 +0530

    feat: add sorting data task 3

---
+++ April-2026/Labs/SQL/Sorting-Data/task-3.sql
@@ -0,0 +1,4 @@
+-- Task: Select name, country, and age from users. Sort first by country (A-Z), then by age (youngest first) within each country.
+
+-- Write your SQL query here
+SELECT name, country, age from USERS ORDER BY country ASC, age ASC
