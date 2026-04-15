71dc74ba5726310bcbeb88a8683be5ef01ea7607
GitPirates <starshivamverma2@gmail.com>
2026-04-15 15:01:34 +0530

    feat: add sorting data task 1

---
+++ April-2026/Labs/SQL/Sorting-Data/task-1.sql
@@ -0,0 +1,5 @@
+-- ORDER BY sorts results. ASC is the default (low to high). This helps find the cheapest products.
+-- Task: Select all columns from the products table and sort by price in ascending order (cheapest first).
+
+-- Write your SQL query here
+SELECT * FROM PRODUCTS ORDER BY price
