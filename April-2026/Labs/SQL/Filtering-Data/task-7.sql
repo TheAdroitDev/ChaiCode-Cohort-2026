bab8d11978050ecdc441890ad09d23ce3897fdf0
GitPirates <starshivamverma2@gmail.com>
2026-04-15 15:02:31 +0530

    feat: add filtering data task 7

---
+++ April-2026/Labs/SQL/Filtering-Data/task-7.sql
@@ -0,0 +1,6 @@
+-- BETWEEN Operator
+
+-- Task: Select name and price from products where price is between 20 and 100 (inclusive).
+
+-- Write your SQL query here
+SELECT name, price from products WHERE price BETWEEN 20 AND 100 -- it will include 100 too
