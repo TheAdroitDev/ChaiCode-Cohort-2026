551a81d877b3d23371199bb7823ec38c456539ed
GitPirates <starshivamverma2@gmail.com>
2026-04-15 15:02:16 +0530

    feat: add filtering data task 4

---
+++ April-2026/Labs/SQL/Filtering-Data/task-4.sql
@@ -0,0 +1,6 @@
+-- Task: Select name and country from users where country is 'Canada' OR country is 'UK'.
+
+-- Write your SQL query here
+SELECT name, country FROM USERS WHERE country = 'Canada' OR country = 'UK' 
+
+-- OR can use IN operator
