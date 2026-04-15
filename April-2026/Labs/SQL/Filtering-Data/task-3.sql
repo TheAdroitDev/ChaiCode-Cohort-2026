488cd19ff61a4bc49e63dc5fb6cff7bf6117b7c3
GitPirates <starshivamverma2@gmail.com>
2026-04-15 15:02:12 +0530

    feat: add filtering data task 3

---
+++ April-2026/Labs/SQL/Filtering-Data/task-3.sql
@@ -0,0 +1,4 @@
+-- Task: Select name, country, and age from users where country is 'USA' AND age is less than 32.
+
+-- Write your SQL query here
+SELECT name, country, age FROM USERS WHERE country = 'USA' AND age < 32
