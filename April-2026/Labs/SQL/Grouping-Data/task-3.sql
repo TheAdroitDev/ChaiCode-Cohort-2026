db81f6cf9d26c898914e6b8d0130f4a750a63caa
GitPirates <starshivamverma2@gmail.com>
2026-04-18 22:04:33 +0530

    feat: add grouping data task 3

---
+++ April-2026/Labs/SQL/Grouping-Data/task-3.sql
@@ -0,0 +1,5 @@
+-- AVG computes the mean value. This shows the typical employee salary in our company.
+
+-- Task: Calculate the average salary of all employees.
+
+SELECT AVG(salary) from employees
