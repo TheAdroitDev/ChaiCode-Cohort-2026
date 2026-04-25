98ce03556fa6164d3e2f37e3774c3f5cceb7fb0f
GitPirates <starshivamverma2@gmail.com>
2026-04-25 23:50:42 +0530

    feat: light setup for JS Mongo Auth

---
+++ April-2026/PRACTICE/Authentication/JS-MongoDB/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+const app = express()
+
+const PORT = 3000
+
+app.get('/', (req, res) => {
+    res.json('Hello World!')
+})
+
+app.routes("/auth",authRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`)
+})  
