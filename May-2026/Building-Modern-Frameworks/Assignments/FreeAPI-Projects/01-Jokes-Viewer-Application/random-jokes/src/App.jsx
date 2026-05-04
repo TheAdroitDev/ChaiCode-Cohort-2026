d3d9fab59f9f6e057cb62f5aa6f558ff2713edbe
GitPirates <starshivamverma2@gmail.com>
2026-05-04 15:38:49 +0530

    feat: import joke component

--- May-2026/Building-Modern-Frameworks/Assignments/FreeAPI-Projects/01-Jokes-Viewer-Application/random-jokes/src/App.jsx
+++ May-2026/Building-Modern-Frameworks/Assignments/FreeAPI-Projects/01-Jokes-Viewer-Application/random-jokes/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import Jokes from './component/Jokes.jsx'
+import Jokes from './components/Jokes.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
