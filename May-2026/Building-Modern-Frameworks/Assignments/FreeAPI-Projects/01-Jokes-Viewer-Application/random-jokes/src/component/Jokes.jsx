6cef6a6fac35ea63d8c9102865ad64a57d1a8df8
GitPirates <starshivamverma2@gmail.com>
2026-05-04 15:17:53 +0530

    feat: add Joke component

---
+++ May-2026/Building-Modern-Frameworks/Assignments/FreeAPI-Projects/01-Jokes-Viewer-Application/random-jokes/src/component/Jokes.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useEffect, useState } from "react";
+
+const Jokes = () => {
+    const [joke, setjoke] = useState()
+    useEffect(() => {
+        
+    }, [])
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Jokes
