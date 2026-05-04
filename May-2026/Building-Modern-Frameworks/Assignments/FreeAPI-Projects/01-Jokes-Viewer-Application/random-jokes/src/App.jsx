fcb2c33b2683d1731a89685d147274b44f034d48
GitPirates <starshivamverma2@gmail.com>
2026-05-04 15:17:45 +0530

    feat: basic vite react setup for Joke application

---
+++ May-2026/Building-Modern-Frameworks/Assignments/FreeAPI-Projects/01-Jokes-Viewer-Application/random-jokes/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Jokes from './component/Jokes.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <h1>Jokes Viewer Application</h1>
+        <Jokes/>
+    </>
+  )
+}
+
+export default App
