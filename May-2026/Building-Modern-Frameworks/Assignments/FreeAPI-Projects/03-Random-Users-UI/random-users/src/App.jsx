3762e05c6a29739cf80ef5b85225e9ccd82ae737
GitPirates <starshivamverma2@gmail.com>
2026-05-10 16:56:06 +0530

    feat: add user component for the project random users

--- May-2026/Building-Modern-Frameworks/Assignments/FreeAPI-Projects/03-Random-Users-UI/random-users/src/App.jsx
+++ May-2026/Building-Modern-Frameworks/Assignments/FreeAPI-Projects/03-Random-Users-UI/random-users/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 
 import './App.css'
+import User from './components/User'
 
 function App() {
-    const [count, setCount] = useState(0)
 
     return (
         <>
-
+            <h1>
+                Random Users
+            </h1>
+            <User/>
         </>
     )
 }
