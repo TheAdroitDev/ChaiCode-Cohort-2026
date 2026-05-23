c23581d5dd2b8b7345507d8bb92c033d9e9ff7cc
GitPirates <starshivamverma2@gmail.com>
2026-05-23 23:29:57 +0530

    feat: implement state management in ManualForm component

--- May-2026/Building-Modern-Frameworks/Introduction-To-React/foundation-06/src/components/ManualForm.jsx
+++ May-2026/Building-Modern-Frameworks/Introduction-To-React/foundation-06/src/components/ManualForm.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const ROLES = ["Frontend", "Backend", "AI Engineer"];
 
 const ManualForm = () => {
-  return (
-    <div>ManualForm</div>
-  )
+    const [values, setValues] = useState({
+        name: "",
+        email: "",
+        role: "Backend",
+        experience: "",
+        cover:""
+    })
+
+    const [errors, setErrors] = useState({})
+    const [submitted, setSubmitted] = useState(false)
+
+    function set(field) {
+        
+    }
+
+    return (
+        <div>
+            ManualForm
+        </div>
+    )
 }
 
 export default ManualForm
