b5de96855fdd550160d9a884fe19c011c42925b5
GitPirates <starshivamverma2@gmail.com>
2026-05-28 17:21:21 +0530

    feat: implement form handling and validation in HookForm component

--- May-2026/Building-Modern-Frameworks/Introduction-To-React/foundation-06/src/components/HookForm.jsx
+++ May-2026/Building-Modern-Frameworks/Introduction-To-React/foundation-06/src/components/HookForm.jsx
@@ -3,9 +3,40 @@ import { useForm } from "react-hook-form";
 const ROLES = ["Frontend", "Backend", "AI Engineer"];
 
 const HookForm = () => {
-  return (
-    <div>HookForm</div>
-  )
+    const { register, handleSubmit, watch, formState: { errors, isSubmitted, isSubmitSuccessful, isSubmitting } } = useForm({
+        defaultValues: { name: "loid forger", mode: "onTouched" }
+    });
+
+    function sumbit(data) {
+        return new Promise((res) => console.log("submitted"), data)
+    }
+
+    if (isSubmitted) {
+        return (
+            <div>
+                <h1>Form submitted successfully</h1>
+            </div>
+        )
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit(sumbit())}>
+                <label>
+                    Full Name
+                    <input {...register("name", { required: "Name is required" })} type="text" />
+                    {errors.name && <span>{errors.name.message}</span>}
+                </label>
+
+                <label>
+                    Email
+                    <input {...register("email", { required: "Email is required" })} type="email" />
+                    {errors.email && <span>{errors.email.message}</span>}
+                </label>
+
+                <button type="submit" disabled={isSubmitting}>{isSubmitting ? "Submitting": "Submit"}</button>
+            </form>
+        </div>
+    )
 }
 
 export default HookForm
