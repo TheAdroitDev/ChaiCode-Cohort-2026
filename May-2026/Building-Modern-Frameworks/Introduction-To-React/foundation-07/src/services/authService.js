232ba585c22e818c07dc6f828d28f54418e75c22
GitPirates <starshivamverma2@gmail.com>
2026-05-31 00:54:32 +0530

    fix: format authService methods for improved readability

--- May-2026/Building-Modern-Frameworks/Introduction-To-React/foundation-07/src/services/authService.js
+++ May-2026/Building-Modern-Frameworks/Introduction-To-React/foundation-07/src/services/authService.js
@@ -7,15 +7,18 @@ export const authService = {
         tokenStore.set(data)
         return data;
     },
+
     async login({email,password}){
         const {data} = await api.post("/auth/login",{email,password})
         tokenStore.set(data)
         return data;
     },
+
     async logout(){
         await api.post("/auth/logout")
         tokenStore.clear()
     },
+    
     async getProfile(){
         const {data} = await api.get("/auth/profile")
         return data.user
