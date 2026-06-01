4a1ebcc57330494f83c977fc7db091eb6152cfbb
GitPirates <starshivamverma2@gmail.com>
2026-06-01 12:56:24 +0530

    feat: import App component and clean up main.tsx structure

--- May-2026/Building-Modern-Frameworks/React-Practice/src/main.tsx
+++ May-2026/Building-Modern-Frameworks/React-Practice/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import App from './App';
 
-
+// in memory tree - Virtual DOM
 const e = document.getElementById("my-react-container")
+
 createRoot(e!).render(
   <StrictMode>
+    <App/>
   </StrictMode>,
 )
