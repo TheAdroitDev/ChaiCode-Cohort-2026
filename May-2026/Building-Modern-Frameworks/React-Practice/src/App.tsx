be300aaa9a13b8d0c02540d34cd12296e1c704bb
GitPirates <starshivamverma2@gmail.com>
2026-06-02 11:34:19 +0530

    feat: enhance user fetching functionality with loading and error states

--- May-2026/Building-Modern-Frameworks/React-Practice/src/App.tsx
+++ May-2026/Building-Modern-Frameworks/React-Practice/src/App.tsx
@@ -4,22 +4,23 @@ import { useRandomUser } from "./hooks/use-random-user"
 
 function App() {
     let [count, setCount] = useState(0)
-    const { user, fetchRandomUser } = useRandomUser()
+    const { user, fetchRandomUser, isFetching, error} = useRandomUser()
 
     function handleIncreaseButtonClick() {
         setCount(count + 1)
     }
 
-    
     return (
         <div>
             <button onClick={handleIncreaseButtonClick}>Increment</button>
             <h1>Count is {count}</h1>
             <button>Decrement</button>
             <div>
-            <button onClick={fetchRandomUser}>Fetch User</button>
-                {user ? <h1>{user.name.first} &nbsp;{user.name.last}</h1> : "No user found ..."}
+                <button onClick={fetchRandomUser}>Fetch User</button>
+                {user ? (isFetching ? <h1>Fetching...</h1> : <h1>{user.name.first} &nbsp;{user.name.last}</h1>) : ("No user found...")}
             </div>
+
+            {error && <div>{error}</div>}
         </div>
     )
 }
