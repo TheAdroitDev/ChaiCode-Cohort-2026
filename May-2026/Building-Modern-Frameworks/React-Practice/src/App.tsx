140358b13ad32e3c51f65c806d4cc4fa9ba16721
GitPirates <starshivamverma2@gmail.com>
2026-06-02 12:07:59 +0530

    feat: refactor App component by removing unused code and simplifying structure

--- May-2026/Building-Modern-Frameworks/React-Practice/src/App.tsx
+++ May-2026/Building-Modern-Frameworks/React-Practice/src/App.tsx
@@ -1,28 +1,22 @@
 import React, { useState } from 'react'
 // custom hook implementation
-import { useRandomUser } from "./hooks/use-random-user"
+// import { useRandomUser } from "./hooks/use-random-user"
+import { Counter } from './components/counter';
 
 function App() {
-    let [count, setCount] = useState(0)
-    const { user, fetchRandomUser, isFetching, error} = useRandomUser()
-
-    function handleIncreaseButtonClick() {
-        setCount(count + 1)
-    }
+    // const { user, fetchRandomUser, isFetching, error} = useRandomUser()
 
     return (
         <div>
-            <button onClick={handleIncreaseButtonClick}>Increment</button>
-            <h1>Count is {count}</h1>
-            <button>Decrement</button>
-            <div>
+            <Counter />
+            {/*   <div>
                 <button onClick={fetchRandomUser}>Fetch User</button>
                 {user ? (isFetching ? <h1>Fetching...</h1> : <h1>{user.name.first} &nbsp;{user.name.last}</h1>) : ("No user found...")}
             </div>
 
-            {error && <div>{error}</div>}
+            {error && <div>{error}</div>} */}
         </div>
-    )
+            )
 }
 
-export default App
+            export default App
