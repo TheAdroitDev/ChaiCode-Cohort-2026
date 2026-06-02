85e9a81efb245d3e7d7f96953a469900303c8856
GitPirates <starshivamverma2@gmail.com>
2026-06-02 12:17:34 +0530

    feat: refactor App component to remove unused code and implement multiple Counter instances

--- May-2026/Building-Modern-Frameworks/React-Practice/src/App.tsx
+++ May-2026/Building-Modern-Frameworks/React-Practice/src/App.tsx
@@ -1,22 +1,15 @@
-import React, { useState } from 'react'
-// custom hook implementation
-// import { useRandomUser } from "./hooks/use-random-user"
 import { Counter } from './components/counter';
 
 function App() {
-    // const { user, fetchRandomUser, isFetching, error} = useRandomUser()
 
     return (
         <div>
-            <Counter />
-            {/*   <div>
-                <button onClick={fetchRandomUser}>Fetch User</button>
-                {user ? (isFetching ? <h1>Fetching...</h1> : <h1>{user.name.first} &nbsp;{user.name.last}</h1>) : ("No user found...")}
-            </div>
-
-            {error && <div>{error}</div>} */}
+            // single line
+            {new Array(2).fill(null).map(() =>
+                <Counter />
+            )}
         </div>
-            )
+    )
 }
 
-            export default App
+export default App
