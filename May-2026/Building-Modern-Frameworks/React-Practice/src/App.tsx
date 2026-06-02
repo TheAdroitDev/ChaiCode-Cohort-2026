5459b52d5c7364d8ea440132f40159509bf53ffa
GitPirates <starshivamverma2@gmail.com>
2026-06-02 11:18:34 +0530

    feat: clean up App component structure and enhance user fetching functionality

--- May-2026/Building-Modern-Frameworks/React-Practice/src/App.tsx
+++ May-2026/Building-Modern-Frameworks/React-Practice/src/App.tsx
@@ -1,19 +1,27 @@
 import React, { useState } from 'react'
+// custom hook implementation
+import { useRandomUser } from "./hooks/use-random-user"
 
 function App() {
-  let [count, setCount] = useState(0)
+    let [count, setCount] = useState(0)
+    const { user, fetchRandomUser } = useRandomUser()
 
-  function handleIncreaseButtonClick() {
-   setCount(count + 1)
-  }
-  
-  return (
-    <div>
-      <button onClick={handleIncreaseButtonClick}>Increment</button>
-      <h1>Count is {count}</h1>
-      <button>Decrement</button>
-    </div>
-  )
+    function handleIncreaseButtonClick() {
+        setCount(count + 1)
+    }
+
+    
+    return (
+        <div>
+            <button onClick={handleIncreaseButtonClick}>Increment</button>
+            <h1>Count is {count}</h1>
+            <button>Decrement</button>
+            <div>
+            <button onClick={fetchRandomUser}>Fetch User</button>
+                {user ? <h1>{user.name.first} &nbsp;{user.name.last}</h1> : "No user found ..."}
+            </div>
+        </div>
+    )
 }
 
 export default App
