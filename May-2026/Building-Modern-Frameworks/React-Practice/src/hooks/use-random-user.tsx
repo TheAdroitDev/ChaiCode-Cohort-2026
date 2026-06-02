9b02b1923926de04d873449e61586b70fd9ea837
GitPirates <starshivamverma2@gmail.com>
2026-06-02 11:34:26 +0530

    feat: improve useRandomUser hook with enhanced error handling and state management

--- May-2026/Building-Modern-Frameworks/React-Practice/src/hooks/use-random-user.tsx
+++ May-2026/Building-Modern-Frameworks/React-Practice/src/hooks/use-random-user.tsx
@@ -1,83 +1,97 @@
 import * as React from "react"
 
 export interface FreeAPIGetRandomUserResponse {
-  data: {
-    cell: string
-    dob: {
-      age: number
-      date: string
+    data: {
+        cell: string
+        dob: {
+            age: number
+            date: string
+        }
+        email: string
+        gender: string
+        id: number
+        location: {
+            city: string
+            coordinates: {
+                latitude: string
+                longitude: string
+            }
+            country: string
+            postcode: number
+            state: string
+            street: {
+                name: string
+                number: number
+            }
+            timezone: {
+                description: string
+                offset: string
+            }
+        }
+        login: {
+            md5: string
+            password: string
+            salt: string
+            sha1: string
+            sha256: string
+            username: string
+            uuid: string
+        }
+        name: {
+            first: string
+            last: string
+            title: string
+        }
+        nat: string
+        phone: string
+        picture: {
+            large: string
+            medium: string
+            thumbnail: string
+        }
+        registered: {
+            age: number
+            date: string
+        }
     }
-    email: string
-    gender: string
-    id: number
-    location: {
-      city: string
-      coordinates: {
-        latitude: string
-        longitude: string
-      }
-      country: string
-      postcode: number
-      state: string
-      street: {
-        name: string
-        number: number
-      }
-      timezone: {
-        description: string
-        offset: string
-      }
-    }
-    login: {
-      md5: string
-      password: string
-      salt: string
-      sha1: string
-      sha256: string
-      username: string
-      uuid: string
-    }
-    name: {
-      first: string
-      last: string
-      title: string
-    }
-    nat: string
-    phone: string
-    picture: {
-      large: string
-      medium: string
-      thumbnail: string
-    }
-    registered: {
-      age: number
-      date: string
-    }
-  }
-  message: string
-  statusCode: number
-  success: boolean
+    message: string
+    statusCode: number
+    success: boolean
 }
 
-export function useRandomUser(){
+export function useRandomUser() {
     const [user, setUser] = React.useState<null | FreeAPIGetRandomUserResponse["data"]>()
+    const [isFetching, setIsFetching] = React.useState<Boolean>(false)
 
-    async function fetchRandomUser () {
-       const rawResponse = await fetch("https://api.freeapi.app/api/v1/public/randomusers/user/random",{
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
+    const [error, setError] = React.useState<null | string>(null)
+    async function fetchRandomUser() {
+        setIsFetching(true)
+        try {
+            const rawResponse = await fetch("https://api.freeapi.app/api/v1/public/randomusers/user/random", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+
+            const response = await rawResponse.json() as FreeAPIGetRandomUserResponse
+            if (response.data && response.success) {
+                setUser(response.data)
+            }
+            setError(null)
+        } catch (error) {
+            setError("Error Fetching User")
+            // console.log("Error Fetching User", error);
+        } finally {
+            setIsFetching(false)
         }
-       })
 
-       const response = await rawResponse.json() as FreeAPIGetRandomUserResponse 
-       if(response.data && response.success){
-        setUser(response.data)
-       } 
     }
 
     return {
         user,
-        fetchRandomUser
+        fetchRandomUser,
+        isFetching,
+        error
     }
 }
