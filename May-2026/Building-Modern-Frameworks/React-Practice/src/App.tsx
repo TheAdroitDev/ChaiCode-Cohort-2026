f7f2018c1b176ef4e729335c48c1425eecabeed4
GitPirates <starshivamverma2@gmail.com>
2026-06-02 15:52:06 +0530

    feat: refactor App component to implement todo list functionality with dynamic state management

--- May-2026/Building-Modern-Frameworks/React-Practice/src/App.tsx
+++ May-2026/Building-Modern-Frameworks/React-Practice/src/App.tsx
@@ -1,13 +1,40 @@
-import { Counter } from './components/counter';
 import * as React from "react"
 
+export interface Todo {
+    id: string,
+    title: string,
+    isCompleted: boolean
+}
 function App() {
-    const [count, setCount] = React.useState(1)
+
+    const [currentValue, setCurrentValue] = React.useState("");
+    const [todos, setTodos] = React.useState<Todo[]>([])
+
+    function handleAddClickAddButton() {
+        todos.push({
+            id: `${Date.now()}`,
+            title: currentValue,
+            isCompleted: false
+        })
+        setTodos(todos)
+    }
     return (
-        <div>
-            <button onClick={() => { setCount(count + 1) }}>Add Counter</button>
-            {new Array(count).fill(null).map(() => <Counter />)}
-        </div>
+        <>
+            <div>
+
+                <div>
+                    <input value={currentValue} onChange={e => setCurrentValue(e.target.value)
+                    } placeholder="Enter your todo here" type="text" />
+                    <button onClick={handleAddClickAddButton}>Add</button>
+
+                </div>
+                <div>
+                    <ul>
+                        {todos.map(e => <li key={e.id}>{e.title}</li>)}
+                    </ul>
+                </div>
+            </div>
+        </>
     )
 }
 
