c98c684f0ff6ee805cee1c50b54d2a7c20cb3ed6
GitPirates <starshivamverma2@gmail.com>
2026-07-19 21:32:52 +0530

    feat: add JSX guidelines and rules as comments in task-1.jsx

---
+++ May-2026/Building-Modern-Frameworks/React-Practice/Labs/task-1.jsx
@@ -0,0 +1,7 @@
+/* JSX looks like HTML but lives inside JavaScript. Under the hood, <h1>Hello</h1> becomes React.createElement('h1', null, 'Hello').
+
+Key Rules
+Use className instead of class
+Embed JavaScript expressions with curly braces {}
+JSX must return a single root element (use <div> or <> fragments)
+Self-closing tags must end with /> (e.g., <img />, <br />) */
